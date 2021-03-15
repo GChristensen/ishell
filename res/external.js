cff45c924225116df9b0b337c5f35acc58c5d7a7
GChristensen <gchristnsn@gmail.com>
2021-03-15 12:15:16 +0400

    site edit

--- res/external.js
+++ res/external.js
@@ -1,8 +1,18 @@
 if (location.href.includes("github")) {
 
-    document.addEventListener("DOMContentLoaded", function(){
+    function loadScript(file) {
+        let head = document.getElementsByTagName('head')[0];
+        let script = document.createElement('script');
+        script.src = '/ishell/' + file;
+        head.appendChild(script);
+    }
+
+
+    document.addEventListener("DOMContentLoaded", function() {
         let nav = document.getElementById("nav-container");
         nav.parentNode.removeChild(nav);
+        loadScript("lib/jquery.js")
+        loadScript("lib/jquery.toc.js")
     });
 
 
