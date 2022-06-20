cdf49d7386adb3646afb2e1c109da2edb7999843
GChristensen <gchristnsn@gmail.com>
2022-06-20 23:13:36 +0400

    site edit

---
+++ addon/ui/options/external.js
@@ -0,0 +1,6 @@
+if (location.href.includes("github")) {
+    document.addEventListener("DOMContentLoaded", function() {
+        let nav = document.getElementById("nav-container");
+        nav.parentNode.removeChild(nav);
+    });
+}
